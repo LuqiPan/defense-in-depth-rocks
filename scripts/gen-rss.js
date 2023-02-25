84c4e0c0308fc4bb17b0b54745d90324e2c19fa6
Luqi Pan <github@luqipan.com>
2023-02-25 01:33:57 -0800

    Update gen-rss.js settings

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml',
+    title: 'Luqi Pan',
+    site_url: 'https://defense.indepth.rocks',
+    feed_url: 'https://defense.indepth.rocks/feed.xml',
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
