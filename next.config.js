067f070254e71f1009e1981024ddc4c00b133778
Luqi Pan <github@luqipan.com>
2023-02-25 02:42:55 -0800

    Disable image optimization

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-blog',
   themeConfig: './theme.config.js',
+  images: {
+    unoptimized: true,
+  },
   // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
 })
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // any configs you need
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = withNextra(nextConfig)
